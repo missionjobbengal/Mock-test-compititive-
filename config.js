7ab9e06cbcb20541ca911f9e549723cb7800b8a8
missionjobbengal <joysarkar.kmj@gmail.com>
2026-08-27 07:28:59 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const TEST_CONFIG = {
+  title: "WBPSC MISCELLANEOUS PRELIMINARY EXAM - 2026 | FULL LENGTH MOCK TEST-01",
+  totalQuestions: 50,
+  totalMarks: 50,
+  durationMinutes: 40,
+  marksPerCorrect: 1,
+  negativePerWrong: 0.25,
+  marksPerUnanswered: 0
+};

38bfa6057a5dc75a732620dbd0a694d731894302
missionjobbengal <joysarkar.kmj@gmail.com>
2026-09-05 09:42:18 +0530

    Create config.js

---
+++ mock-test-3/config.js
@@ -0,0 +1,9 @@
+const TEST_CONFIG = {
+  title: "MOCK TEST FOR ALL COMPETITIVE EXAMS",
+  totalQuestions: 50,
+  totalMarks: 50,
+  durationMinutes: 40,
+  marksPerCorrect: 1,
+  negativePerWrong: 0.25,
+  marksPerUnanswered: 0
+};
